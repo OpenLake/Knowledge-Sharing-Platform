53a17813fdaa0ae2ca7c73f85dd1afaee84650be
battuAshita <battua@iitbhilai.ac.in>
2022-01-20 21:16:15 +0530

    Added search feature to filter Notes, PYQs etc. by Course ID and Instructor

--- ksp_frontend/src/components/PYQs/NewPaperModal.js
+++ ksp_frontend/src/components/PYQs/NewPaperModal.js
@@ -1,55 +1,57 @@
-import React, { Component, Fragment } from "react";
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import React, {Component, Fragment} from "react";
+import {Button, Modal, ModalHeader, ModalBody} from "reactstrap";
 import NewForm from "../NewForm";
 import "../../App.css";
 
 class NewPaperModal extends Component {
-  state = {
-    modal: false
-  };
+    state = {
+        modal: false
+    };
 
-  toggle = () => {
-    this.setState(previous => ({
-      modal: !previous.modal
-    }));
-  };
+    toggle = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }));
+    };
 
-  render() {
-    const create = this.props.create;
+    render() {
+        const create = this.props.create;
 
-    if (create) {
-      var title = "ADD NEW PAPERS";
+        if (create) {
+            var title = "ADD NEW PAPERS";
 
-      var button = (
-         <Button
-          onClick={this.toggle}
-          style={{ minWidth: "200px", minHeight: "40px", color: "white", backgroundColor: "#3c52ad", position: "fixed"}}
-        >
-          <span> ADD PAPERS </span>
-        </Button>
-      );
+            var button = (
+                <Button
+                    onClick={this.toggle}
+                    className="button-style"
+                    style={{backgroundColor: "#3c52ad"}}
+
+                >
+                    <span> ADD PAPER </span>
+                </Button>
+            );
+        }
+
+        return (
+            <Fragment>
+                <div className="button">
+                    {button}
+                </div>
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}><span>{title}</span></ModalHeader>
+
+                    <ModalBody>
+                        <NewForm
+                            resetState={this.props.resetState}
+                            toggle={this.toggle}
+                            student={this.props.student}
+                        />
+                    </ModalBody>
+                </Modal>
+            </Fragment>
+        );
     }
-
-    return (
-      <Fragment>
-        <div className="button">
-          {button}
-        </div>
-
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}><span>{title}</span></ModalHeader>
-
-          <ModalBody>
-            <NewForm
-              resetState={this.props.resetState}
-              toggle={this.toggle}
-              student={this.props.student}
-            />
-          </ModalBody>
-        </Modal>
-      </Fragment>
-    );
-  }
 }
 
 export default NewPaperModal;
--- ksp_frontend/src/components/PYQs/NoteList.js
+++ ksp_frontend/src/components/PYQs/NoteList.js
@@ -1,46 +1,67 @@
-import React, { Component } from "react";
-import { Table } from "reactstrap";
+import React, {useState} from "react";
+import {Table} from "reactstrap";
 import "../../App.css";
 
 
-class NoteList extends Component {
-  render() {
-    const notes = this.props.notes;
+function NoteList(props) {
+
+    const notes = props.notes;
+    const [searchTerm, setSearchTerm] = useState(" ");
+
     return (
-      <Table dark bordered responsive>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Name</th>
-            <th>Course ID</th>
-            <th>Batch</th>
-            <th>Course Instructor</th>
-            <th>Drive link</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!notes || notes.length <= 0 ? (
-            <tr>
-              <td colSpan="6" align="center" width="400px">
-                <b>Oops, nothing here yet</b>
-              </td>
-            </tr>
-          ) : (
-            notes.map(note => (
-              <tr key={note.pk}>
-                <td>{note.title}</td>
-                <td>{note.name}</td>
-                <td>{note.course_id}</td>
-                <td>{note.batch}</td>
-                <td>{note.course_instructor}</td>
-                <td>{note.gdrive_link}</td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </Table>
+        <>
+            <div className="search-bar">
+                <input type="text"
+                       placeholder="Search"
+                       className="search-input"
+                       onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+            <Table dark bordered responsive>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Name</th>
+                    <th>Course ID</th>
+                    <th>Batch</th>
+                    <th>Course Instructor</th>
+                    <th>Drive link</th>
+                </tr>
+                </thead>
+                <tbody>
+                {!notes || notes.length <= 0 ? (
+                    <tr>
+                        <td colSpan="6" align="center" width="400px">
+                            <b>Oops, nothing here yet</b>
+                        </td>
+                    </tr>
+                ) : (
+                    notes.filter(val => {
+                            if (searchTerm === " ") {
+                                return val;
+                            } else if (
+                                val.course_id.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                val.course_instructor.toString().toLowerCase().includes(searchTerm.toLowerCase())) {
+
+                                return val;
+                            }
+                        }
+                    ).map(note => (
+                        <tr key={note.pk}>
+                            <td>{note.title}</td>
+                            <td>{note.name}</td>
+                            <td>{note.course_id}</td>
+                            <td>{note.batch}</td>
+                            <td>{note.course_instructor}</td>
+                            <td>{note.gdrive_link}</td>
+                        </tr>
+                    ))
+                )}
+                </tbody>
+            </Table>
+        </>
     );
-  }
+
 }
 
 export default NoteList;
