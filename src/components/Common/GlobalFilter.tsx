0157ba4089cf005f9cddb920f576cf71ce5c7763
Subin S K <subinsk284@gmail.com>
2023-01-12 15:36:19 +0530

    search bar css fixed

--- src/components/Common/GlobalFilter.tsx
+++ src/components/Common/GlobalFilter.tsx
@@ -13,6 +13,12 @@ export const GlobalFilter: FC<{
 
     return (
         <div className="flex w-full px-2 py-3">
+            <label
+                htmlFor="search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only"
+            >
+                Search
+            </label>
             <div className="relative w-full">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg
@@ -39,7 +45,7 @@ export const GlobalFilter: FC<{
                     }}
                     type="search"
                     id="search"
-                    className="w-full p-2 md:p-4 font-medium text-base pl-10  text-gray-700 outline-none ring-2 ring-primary/40 focus:border-none rounded-md bg-primary/5 focus:bg-primary/10 focus:ring-primary placeholder:font-medium placeholder:text-gray-600"
+                    className="w-full font-medium text-base pl-12 pr-2 py-2 md:pr-4 md:py-4 text-gray-700 outline-none ring-2 ring-primary/40 focus:border-none rounded-md bg-primary/5 focus:bg-primary/10 focus:ring-primary placeholder:font-medium placeholder:text-gray-600"
                     placeholder={`Search ${records} records...`}
                 />
             </div>
