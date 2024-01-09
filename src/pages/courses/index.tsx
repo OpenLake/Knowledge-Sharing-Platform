49119f3be2f6ab86d01ed0b8301e11e551fdf517
Gunj Joshi <gunjjoshi@Gunj-Joshis-MacBook.local>
2024-01-09 12:01:02 +0530

    aligned the search icon

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -118,7 +118,7 @@ export default function Courses() {
                         Search
                     </label>
                     <div className="relative w-full">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none" style={{"marginTop":"-5%"}}>
                             <svg
                                 aria-hidden="true"
                                 className="w-5 h-5 text-primary"
