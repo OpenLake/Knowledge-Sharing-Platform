d7069819366361d3b6cf37cd259f3757371beb0f
GUNJ JOSHI <gunjjoshi8372@gmail.com>
2024-02-29 16:46:05 +0530

    Update awsConfig.ts

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -5,9 +5,9 @@ process.env.AWS_SDK_LOAD_CONFIG = '1'
 process.env.AWS_CONFIG_FILE = '/Knowledge-Sharing-Platform/src/config/index.ts'
 
 AWS.config.update({
-    accessKeyId: 'AKIA3FLD3K3RPXVRMDOK',
-    secretAccessKey: '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',
-    region: 'eu-north-1',
+    accessKeyId: '',
+    secretAccessKey: '8',
+    region: '',
 })
 
 export const s3 = new AWS.S3()
