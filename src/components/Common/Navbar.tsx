61be4c8d46fde8b27cab7f5643326358654c2f3f
jhansilakshmi27 <matchajh@iitbhilai.ac.in>
2025-06-22 19:28:46 +0530

    removed other features

--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -121,13 +121,6 @@ export const Navbar: FC<{}> = ({}) => {
                 >
                     Rate Professor
                 </span>
-                 <span
-                    ref={router.asPath === '/template' ? activeRef : null}
-                    onClick={() => router.push('/template')}
-                    className={`cursor-pointer p-3 relative before:content-[''] before:absolute before:bottom-[-7px] before:left-0 before:w-full before:h-[5px] before:bg-primary/40 before:rounded-[8px_8px_0_0] before:opacity-0 before:duration-100 text-gray-600 hover:before:opacity-100 hover:before:bottom-0 hover:text-gray-800`}
-                >
-                    Templates
-                </span>
                 <span
                     style={{
                         width: `${activeOffsetWidth}px`,
